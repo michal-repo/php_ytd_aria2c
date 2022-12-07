c669957483d33424ec9aeb5dad65ef49a83c8adb
Michal <salin87@gmail.com>
2022-12-07 23:14:16 +0100

    php 2 aria2c

---
+++ aria2.php
@@ -0,0 +1,281 @@
+<?php
+
+// https://github.com/shiny/php-aria2
+
+class Aria2
+{
+    protected $ch;
+    protected $token;
+    protected $batch = false;
+    protected $batch_cmds = [];
+
+    function __construct($server = 'http://127.0.0.1:6800/jsonrpc', $token = null)
+    {
+        $this->ch = curl_init($server);
+        $this->token = $token;
+        curl_setopt_array($this->ch, [
+            CURLOPT_POST => true,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_HEADER => false
+        ]);
+    }
+
+    function __destruct()
+    {
+        curl_close($this->ch);
+    }
+
+    protected function req($data)
+    {
+        curl_setopt($this->ch, CURLOPT_POSTFIELDS, $data);
+        return curl_exec($this->ch);
+    }
+
+    function batch($func = null)
+    {
+        $this->batch = true;
+        if (is_callable($func)) {
+            $func($this);
+        }
+        return $this;
+    }
+
+    function inBatch()
+    {
+        return $this->batch;
+    }
+
+    function commit()
+    {
+        $this->batch = false;
+        $cmds = json_encode($this->batch_cmds);
+        $result = $this->req($cmds);
+        $this->batch_cmds = [];
+        return $result;
+    }
+
+    function __call($name, $arg)
+    {
+        if (!is_null($this->token)) {
+            array_unshift($arg, $this->token);
+        }
+
+        //Support system methods
+        if (strpos($name, '_') === false) {
+            $name = 'aria2.' . $name;
+        } else {
+            $name = str_replace('_', '.', $name);
+        }
+
+        $data = [
+            'jsonrpc' => '2.0',
+            'id' => '1',
+            'method' => $name,
+            'params' => $arg
+        ];
+        //Support batch requests
+        if ($this->batch) {
+            $this->batch_cmds[] = $data;
+            return $this;
+        }
+        $data = json_encode($data);
+        $response = $this->req($data);
+        if ($response === false) {
+            trigger_error(curl_error($this->ch));
+        }
+        return json_decode($response, 1);
+    }
+}
+
+class php2Aria2c
+{
+    private $url;
+    private $selectedFormat;
+    private $formats;
+    private $cookiesId;
+    private $cookiesPath;
+    private $out;
+    private $dir;
+    private $opt;
+    private $connection;
+
+    function __construct($url = "", $selectedFormat = null, $formats = null, $outName = null, $dirName = null)
+    {
+        $this->url = $url;
+        $this->selectedFormat = $selectedFormat;
+        $this->formats = $formats;
+        $this->cookiesId = md5($this->url);
+        $this->cookiesPath = '"cookies/' . $this->cookiesId . '.cookiesjar"';
+        $this->out = $outName;
+        $this->dir = $dirName;
+        $this->opt = array();
+        $this->initDB();
+    }
+
+    public function fetchFormats()
+    {
+        $this->formats = array();
+        $out = shell_exec('youtube-dl "' . $this->url . '" -F');
+        $lines = explode(PHP_EOL, $out);
+        $start = false;
+        foreach ($lines as $line) {
+            if (strpos($line, "format") !== false && strpos($line, "resolution") !== false && strpos($line, "extension") !== false && !$start) {
+                $start = true;
+                continue;
+            } elseif (!$start) {
+                continue;
+            }
+
+            $format_code = substr($line, 0, strpos($line, " "));
+            $this->formats[$line] = $format_code;
+        }
+        return $this;
+    }
+
+    public function getFormats()
+    {
+        return $this->formats;
+    }
+
+
+    public function setSelectedFormat($format)
+    {
+        $this->selectedFormat = $format;
+    }
+
+    public function setOutName($outName)
+    {
+        $this->out = $outName;
+    }
+
+    public function setDirName($dirName)
+    {
+        $this->dir = $dirName;
+    }
+
+    private function generateOptionsForAria2c()
+    {
+        $this->opt['out'] = $this->out;
+        $this->opt['dir'] = $this->dir;
+        $this->opt['load-cookies'] = $this->cookiesPath;
+    }
+
+    public function addToInternalQueue()
+    {
+        if (!is_null($this->connection)) {
+            $this->generateOptionsForAria2c();
+            $stmt = $this->connection->prepare("insert into downloads (url, formatOption, cookiesPath, opt, addedTime) values (:url, :formatOption, :cookiesPath, :opt, datetime('now', 'localtime'))");
+            $stmt->bindParam(':url', $this->url);
+            $stmt->bindParam(':formatOption', $this->selectedFormat);
+            $stmt->bindParam(':cookiesPath', $this->cookiesPath);
+            $stmt->bindParam(':opt', serialize($this->opt));
+            $stmt->execute();
+            $status = "Added to internal queue";
+        } else {
+            $status = "Unable to connect to local database.";
+        }
+        return $status;
+    }
+
+    public static function processOneElementFromInternalQueue(){
+        $self = new self();
+        return $self->pProcessOneElementFromInternalQueue();
+    }
+
+    public function pProcessOneElementFromInternalQueue(){
+        if (!is_null($this->connection)) {
+            $stmt = $this->connection->prepare("select * from downloads where dispatched = 0 order by id asc limit 1");
+            $stmt->execute();
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            $this->url = $data['url'];
+            $this->selectedFormat = $data['formatOption'];
+            $this->cookiesPath = $data['cookiesPath'];
+            $this->opt = unserialize($data['opt']);
+            list($status, $code) = self::checkAria2cDispatchStatus(self::dispatchToAira2c());
+            if ($code === 1){
+                $stmt = $this->connection->prepare("update downloads set dispatched = 1 where id = :id");
+                $stmt->bindParam(':id', $data['id']);
+                $stmt->execute();
+            }
+        } else {
+            $status = "Unable to connect to local database.";
+        }
+        return $status;
+    }
+
+    public static function removeDispatchedURLsFromInternalQueue(){
+        $self = new self();
+        return $self->pRemoveDispatchedURLsFromInternalQueue();
+    }
+
+    public function pRemoveDispatchedURLsFromInternalQueue(){
+        $stmt = $this->connection->prepare("delete from downloads where dispatched = 1");
+        $stmt->execute();
+        return true;
+    }
+
+    public function addToAria2cQueue()
+    {
+        $this->generateOptionsForAria2c();
+        list($status, $unused) = $this->checkAria2cDispatchStatus($this->dispatchToAira2c());
+        return $status;
+    }
+
+    private function checkAria2cDispatchStatus($out){
+        if (isset($out['id'])) {
+            $status = "Added to aria2c queue";
+            $code = 1;
+        } else {
+            $status = "Unable to add URL to aria2c queue";
+            $code = 0;
+        }
+        return array($status, $code);
+    }
+
+    private function dispatchToAira2c(){
+        $url = shell_exec('youtube-dl -f "' . $this->selectedFormat . '" "' . $this->url . '" -g --cookies ' . $this->cookiesPath);
+        $aria2 = new Aria2();
+        $out = $aria2->addUri(
+            [$url],
+            $this->opt
+        );
+        return $out;
+    }
+
+    private function initDB()
+    {
+        try {
+            $initDB = false;
+            if (!is_file(__DIR__ . "/php_aria2.db")) {
+                $initDB = true;
+            }
+            $this->connection = new PDO("sqlite:" . __DIR__ . "/php_aria2.db");
+            if ($initDB) {
+                $stmt = $this->connection->prepare('create table downloads (id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT NOT NULL, formatOption TEXT NOT NULL, cookiesPath TEXT NOT NULL, opt TEXT NOT NULL, dispatched INTEGER DEFAULT 0 NOT NULL, addedTime TEXT NOT NULL) ');
+                $stmt->execute();
+            }
+        } catch (Exception $e) {
+            $this->connection = NULL;
+            echo "Unable to connect to local database.<br>" . $e->getMessage();
+        }
+    }
+
+    public static function getCountFromInternalQueue(){
+        $self = new self();
+        return $self->pGetCountFromInternalQueue();
+    }
+
+    public function pGetCountFromInternalQueue(){
+        if (!is_null($this->connection)) {
+            $stmt = $this->connection->prepare("select count(*) as count from downloads where dispatched = 0");
+            $stmt->execute();
+            $data = $stmt->fetch(PDO::FETCH_ASSOC);
+            $status = $data['count'] . " items in internal queue.";
+        } else {
+            $status = "Unable to connect to local database.";
+        }
+        return $status;
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,176 @@
+<?php
+
+require "aria2.php";
+
+$default_name_value = "";
+
+function redirectToSelf($opt = "")
+{
+    header("Location: " . htmlspecialchars($_SERVER["PHP_SELF"]) . $opt);
+    die();
+}
+
+if (isset($_POST['url'])) {
+    $url = $_POST['url'];
+    if (!empty($url)) {
+        $php2Aria2c = new php2Aria2c($url);
+        $available_formats = $php2Aria2c->fetchFormats()->getFormats();
+    }
+}
+
+if (isset($php2Aria2c) && isset($_POST['formatOption']) && in_array($_POST['formatOption'], $available_formats)) {
+    $php2Aria2c->setSelectedFormat($_POST['formatOption']);
+    if (isset($_POST['out_name']) && $_POST['out_name'] !== $default_name_value) {
+        $php2Aria2c->setOutName($_POST['out_name']);
+    }
+    if (isset($_POST['dir_name']) && !empty($_POST['dir_name'])) {
+        $php2Aria2c->setDirName($_POST['dir_name']);
+    }
+    if ($_POST['addToInternalQueue'] == true) {
+        $status = $php2Aria2c->addToInternalQueue();
+    } else {
+        $status = $php2Aria2c->addToAria2cQueue();
+    }
+    redirectToSelf(("?status=" . urlencode($status)));
+} elseif (isset($_POST['formatOption'])) {
+    echo "Selected invalid format";
+}
+
+if (isset($_POST['cleanUpCookies']) && $_POST['cleanUpCookies'] === "yes") {
+    $files = glob('cookies/*');
+    foreach ($files as $file) {
+        if (is_file($file)) {
+            unlink($file);
+        }
+    }
+    php2Aria2c::removeDispatchedURLsFromInternalQueue();
+    redirectToSelf(("?status=" . urlencode("Cleaned up!")));
+}
+
+if (isset($_POST['processOneFromInternalQueue']) && $_POST['processOneFromInternalQueue'] === "yes") {
+    $status = php2Aria2c::processOneElementFromInternalQueue();
+    redirectToSelf(("?status=" . urlencode($status)));
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <title>ytd aria2c UI</title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="..\bootstrap\css\bootstrap.min.css">
+    <script src="..\bootstrap\js\bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <?php if (isset($_GET['status'])) { ?>
+            <div class="row">
+                <div class="col">
+                    <div class="alert alert-success" role="alert">
+                        <?php echo urldecode($_GET['status']) ?>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+        <div class="row">
+            <div class="col">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method='POST'>
+                    <div class="mb-3">
+                        <label for="url" class="form-label">URL</label>
+                        <input type="text" class="form-control" id="url" name="url" aria-describedby="urlHelp" <?php
+                                                                                                                if (isset($_POST['url'])) {
+                                                                                                                    echo 'value="' . $_POST['url'] . '"';
+                                                                                                                }
+                                                                                                                ?>>
+                        <div id="urlHelp" class="form-text">URL to download.</div>
+                        <label for="out_name" class="form-label">Save as</label>
+                        <input type="text" class="form-control" id="out_name" name="out_name" aria-describedby="out_nameHelp" <?php
+                                                                                                                                if (isset($_POST['out_name'])) {
+                                                                                                                                    echo 'value="' . $_POST['out_name'] . '"';
+                                                                                                                                } else {
+                                                                                                                                    echo 'value="' . $default_name_value . '"';
+                                                                                                                                }
+                                                                                                                                ?>>
+                        <div id="out_nameHelp" class="form-text">Specify custom name (optional), will save to default name returned by URL if not specified.</div>
+                        <label for="dir_name" class="form-label">Save location</label>
+                        <input type="text" class="form-control" id="dir_name" name="dir_name" aria-describedby="dir_nameHelp" <?php
+                                                                                                                                if (isset($_POST['dir_name'])) {
+                                                                                                                                    echo 'value="' . $_POST['dir_name'] . '"';
+                                                                                                                                }
+                                                                                                                                ?>>
+                        <div id="dir_nameHelp" class="form-text">Specify custom save location (optional), will save to default from aria2c config if not specified.</div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="true" id="addToInternalQueue" name="addToInternalQueue" aria-describedby="addToInternalQueueHelp" <?php if (isset($_POST['addToInternalQueue']) && $_POST['addToInternalQueue'] === "true") {
+                                                                                                                                                                                            echo "checked";
+                                                                                                                                                                                        } ?> <label class="form-check-label" for="addToInternalQueue">
+                            Add to internal queue
+                            </label>
+                        </div>
+                        <div id="addToInternalQueueHelp" class="form-text">If checked, URL will be added to internal queue, URL will be prepared and added to aria2c if there will be empty download slot.</br>Useful if you download fails because of expired URL from source page.</div>
+
+                        <?php
+                        if (isset($available_formats) && is_array($available_formats)) {
+                        ?>
+                            <hr>
+                            <label for="formatOption" class="form-label">Format options, select one</label>
+                            <?php
+                            foreach ($available_formats as $name => $format) {
+                                if (empty($format) && $format !== "0") {
+                                    continue;
+                                }
+                            ?>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="formatOption" id="<?php echo $format; ?>" value="<?php echo $format; ?>" <?php if (isset($_POST['formatOption']) && $_POST['formatOption'] === $format) {
+                                                                                                                                                                    echo "checked";
+                                                                                                                                                                } ?>>
+                                    <label class="form-check-label" for="<?php echo $format; ?>">
+                                        <?php echo $name; ?>
+                                    </label>
+                                </div>
+                        <?php
+                            }
+                        }
+                        ?>
+                    </div>
+                    <button type="submit" class="btn btn-primary"><?php if (isset($available_formats)) {
+                                                                        echo "Add to queue";
+                                                                    } else {
+                                                                        echo "Get formats";
+                                                                    } ?></button>
+                </form>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method='POST' class="float-end">
+                    <input type="hidden" value="yes" id="cleanUpCookies" name="cleanUpCookies">
+                    <button type="submit" class="btn btn-danger">Clean up cookies & dispatched jobs</button>
+                </form>
+            </div>
+        </div>
+        </br>
+        <div class="row">
+            <div class="col">
+                <h6 class="float-end"><?php echo php2Aria2c::getCountFromInternalQueue() ?></h6>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method='POST' class="float-end">
+                    <input type="hidden" value="yes" id="processOneFromInternalQueue" name="processOneFromInternalQueue">
+                    <button type="submit" class="btn btn-success">Process one URL from Queue</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+
+</html>
